9ee2d7d9bb98a6f9ce1a503b90ec2402fadb37e7
anya-hichu <anya_hichu@outlook.com>
2025-12-10 23:03:52 +0100

    Fix typo

--- ModOrganizer/Windows/States/Results/Visibles/IVisibleResultStateExtensions.cs
+++ ModOrganizer/Windows/States/Results/Visibles/IVisibleResultStateExtensions.cs
@@ -3,7 +3,7 @@ using System.Linq;
 
 namespace ModOrganizer.Windows.States.Results.Visibles;
 
-public static class IVisibleResultStateExtension
+public static class IVisibleResultStateExtensions
 {
     public static IReadOnlyDictionary<string, IVisibleResult> GetVisibleResultByModDirectory(this IVisibleResultState visibleResultState) => visibleResultState.GetResultByModDirectory<IVisibleResult>().Where(p => p.Value.IsVisible(visibleResultState)).ToDictionary();
 }
