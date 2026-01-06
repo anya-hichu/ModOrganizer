63025d491fbd7741d19c8dd908fec1d4f64bbe2f
anya-hichu <anya_hichu@outlook.com>
2026-01-06 20:29:03 +0100

    wtf

--- ModOrganizer.Tests/Json/Penumbra/DefaultMods/DefaultModReaderBuilder.cs
+++ ModOrganizer.Tests/Json/Penumbra/DefaultMods/DefaultModReaderBuilder.cs
@@ -5,6 +5,7 @@ using ModOrganizer.Json.Penumbra.DefaultMods;
 using ModOrganizer.Json.Readers.Asserts.Fakes;
 using ModOrganizer.Json.Readers.Elements.Fakes;
 using ModOrganizer.Json.Readers.Fakes;
+using ModOrganizer.Shared;
 using ModOrganizer.Tests.Dalamuds.PluginLogs;
 using ModOrganizer.Tests.Json.Penumbra.Containers;
 using ModOrganizer.Tests.Json.Readers.Asserts;
@@ -12,12 +13,12 @@ using ModOrganizer.Tests.Json.Readers.Elements;
 
 namespace ModOrganizer.Tests.Json.Penumbra.DefaultMods;
 
-public class DefaultModReaderBuilder : Builder<DefaultModReader>, IStubbableAssert, IStubbableContainerReader, IStubbableElementReader, IStubbablePluginLog
+public class DefaultModReaderBuilder : IBuilder<DefaultModReader>, IStubbableAssert, IStubbableContainerReader, IStubbableElementReader, IStubbablePluginLog
 {
     public StubIAssert AssertStub { get; init; } = new() { InstanceBehavior = StubBehaviors.NotImplemented };
     public StubIReader<Container> ContainerReaderStub { get; init; } = new() { InstanceBehavior = StubBehaviors.NotImplemented };
     public StubIElementReader ElementReaderStub { get; init; } = new() { InstanceBehavior = StubBehaviors.NotImplemented };
     public StubIPluginLog PluginLogStub { get; init; } = new() { InstanceBehavior = StubBehaviors.NotImplemented };
 
-    public override DefaultModReader Build() => new(AssertStub, ContainerReaderStub, ElementReaderStub, PluginLogStub);
+    public DefaultModReader Build() => new(AssertStub, ContainerReaderStub, ElementReaderStub, PluginLogStub);
 }
