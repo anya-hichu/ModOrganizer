7fd7045d8c495ac2ca030a0baa2ada8265966edd
anya-hichu <anya_hichu@outlook.com>
2026-01-06 11:40:45 +0100

    Stuff

--- ModOrganizer/Virtuals/VirtualMultiMatcher.cs
+++ ModOrganizer/Virtuals/VirtualMultiMatcher.cs
@@ -3,7 +3,7 @@ using System.Linq;
 
 namespace ModOrganizer.Virtuals;
 
-public class VirtualMultiMatcher(List<VirtualMatcher> matchers) : VirtualMatcher
+public class VirtualMultiMatcher(IEnumerable<VirtualMatcher> matchers) : VirtualMatcher
 {
     public override bool Matches(VirtualFolder folder) => matchers.All(m => m.Matches(folder));
 
