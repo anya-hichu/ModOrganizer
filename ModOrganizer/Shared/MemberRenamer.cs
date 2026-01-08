3f62498b9a6ba0da34ddfc60b5946f59ad514431
anya-hichu <anya_hichu@outlook.com>
2026-01-08 06:38:23 +0100

    oops

--- ModOrganizer/Shared/MemberRenamer.cs
+++ ModOrganizer/Shared/MemberRenamer.cs
@@ -1,10 +1,9 @@
 using Scriban.Runtime;
 using System.Reflection;
-using System.Xml.Linq;
 
 namespace ModOrganizer.Shared;
 
 public static class MemberRenamer
 {
-    public static string Rename(MemberInfo memberInfo) => StandardMemberRenamer.Rename(memberInfo.Name).Trim('_');
+    public static string Rename(MemberInfo memberInfo) => StandardMemberRenamer.Rename(memberInfo).Trim('_');
 }
