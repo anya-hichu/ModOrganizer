1256ab0dfc17346ff9d41d7332cf40a79d8839ca
anya-hichu <anya_hichu@outlook.com>
2026-01-10 15:15:08 +0100

    Rename

--- ModOrganizer.Shims/Dalamud.Bindings.ImGui/ImGui.cs
+++ ModOrganizer.Shims/Dalamud.Bindings.ImGui/ImGui.cs
@@ -6,6 +6,7 @@ public class ImGui
 {
     public static IImGui? Instance { get; }
 
+    // Dispatch to instance to allow stubbing
     public static void Text(ImU8String text) => Instance?.Text(text);
     public static bool Button(ImU8String label, Vector2 size) => Instance != null && Instance.Button(label, size);
 }
--- ModOrganizer.Shims/Dalamud.Bindings.ImGui/ImU8String.cs
+++ ModOrganizer.Shims/Dalamud.Bindings.ImGui/ImU8String.cs
@@ -1,10 +1,4 @@
-using System;
-using System.Buffers;
-using System.Diagnostics;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Text.Unicode;
 
 namespace Dalamud.Bindings.ImGui;
 
