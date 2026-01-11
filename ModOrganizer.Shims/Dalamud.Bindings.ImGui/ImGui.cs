fd232a9b2d00106d552db685cd6e477f37dff807
anya-hichu <anya_hichu@outlook.com>
2026-01-11 23:19:57 +0100

    fix typo

--- ModOrganizer.Shims/Dalamud.Bindings.ImGui/ImGui.cs
+++ ModOrganizer.Shims/Dalamud.Bindings.ImGui/ImGui.cs
@@ -9,35 +9,35 @@ public static class ImGui
 
     public static IImGui? MaybeImplementation { get; set; }
 
-    public static bool Button(ImU8String label, Vector2 size) => GetImplemention().Button(label, size);
+    public static bool Button(ImU8String label, Vector2 size) => GetImplementation().Button(label, size);
 
-    public static bool Checkbox(ImU8String label, ref bool value) => GetImplemention().Checkbox(label, ref value);
+    public static bool Checkbox(ImU8String label, ref bool value) => GetImplementation().Checkbox(label, ref value);
 
-    public static uint GetColorU32(ImGuiCol idx) => GetImplemention().GetColorU32(idx);
-    public static float GetWindowWidth() => GetImplemention().GetWindowWidth();
+    public static uint GetColorU32(ImGuiCol idx) => GetImplementation().GetColorU32(idx);
+    public static float GetWindowWidth() => GetImplementation().GetWindowWidth();
     
     public static bool InputText(ImU8String label, scoped ref string buf, int maxLength = ImU8String.AllocFreeBufferSize, ImGuiInputTextFlags flags = ImGuiInputTextFlags.None, ImGuiInputTextCallbackDelegate? callback = null) =>
-        GetImplemention().InputText(label, ref buf, maxLength, flags, callback);
+        GetImplementation().InputText(label, ref buf, maxLength, flags, callback);
     public static bool InputTextWithHint(ImU8String label, ImU8String hint, scoped ref string buf, int maxLength = ImU8String.AllocFreeBufferSize, ImGuiInputTextFlags flags = ImGuiInputTextFlags.None, ImGuiInputTextCallbackDelegate? callback = null) =>
-        GetImplemention().InputTextWithHint(label, hint, ref buf, maxLength, flags, callback);
+        GetImplementation().InputTextWithHint(label, hint, ref buf, maxLength, flags, callback);
     
-    public static ImGuiListClipperPtr ImGuiListClipper() => GetImplemention().ImGuiListClipper();
+    public static ImGuiListClipperPtr ImGuiListClipper() => GetImplementation().ImGuiListClipper();
 
-    public static bool IsItemClicked() => GetImplemention().IsItemClicked();
-    public static bool IsItemHovered() => GetImplemention().IsItemHovered();
+    public static bool IsItemClicked() => GetImplementation().IsItemClicked();
+    public static bool IsItemHovered() => GetImplementation().IsItemHovered();
 
-    public static void PopStyleColor(int count) => GetImplemention().PopStyleColor(count);
-    public static void PushStyleColor(ImGuiCol idx, Vector4 col) => GetImplemention().PushStyleColor(idx, col);
+    public static void PopStyleColor(int count) => GetImplementation().PopStyleColor(count);
+    public static void PushStyleColor(ImGuiCol idx, Vector4 col) => GetImplementation().PushStyleColor(idx, col);
 
-    public static void SameLine() => GetImplemention().SameLine();
-    public static void SameLine(float offsetFromStartX) => GetImplemention().SameLine(offsetFromStartX);
+    public static void SameLine() => GetImplementation().SameLine();
+    public static void SameLine(float offsetFromStartX) => GetImplementation().SameLine(offsetFromStartX);
 
-    public static void SetTooltip(ImU8String text) => GetImplemention().SetTooltip(text);
+    public static void SetTooltip(ImU8String text) => GetImplementation().SetTooltip(text);
 
-    public static void Text(ImU8String text) => GetImplemention().Text(text);
+    public static void Text(ImU8String text) => GetImplementation().Text(text);
 
-    public static bool TreeNodeEx(ImU8String id, ImGuiTreeNodeFlags flags = ImGuiTreeNodeFlags.None, ImU8String label = default) => GetImplemention().TreeNodeEx(id, flags, label);
-    public static void TreePop() => GetImplemention().TreePop();
+    public static bool TreeNodeEx(ImU8String id, ImGuiTreeNodeFlags flags = ImGuiTreeNodeFlags.None, ImU8String label = default) => GetImplementation().TreeNodeEx(id, flags, label);
+    public static void TreePop() => GetImplementation().TreePop();
 
-    private static IImGui GetImplemention() => MaybeImplementation == null ? throw new NotImplementedException() : MaybeImplementation;
+    private static IImGui GetImplementation() => MaybeImplementation ?? throw new NotImplementedException();
 }
