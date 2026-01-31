fcccc5a8faa5638f1e0ad5162c1c67bad1fd19f2
anya-hichu <anya_hichu@outlook.com>
2026-01-31 17:53:40 +0100

    tests

--- ModOrganizer.Tests/Json/Penumbra/Groups/Combinings/TestGroupCombiningReader.cs
+++ ModOrganizer.Tests/Json/Penumbra/Groups/Combinings/TestGroupCombiningReader.cs
@@ -1,4 +1,7 @@
+using Dalamud.Plugin.Services;
+using Microsoft.QualityTools.Testing.Fakes.Stubs;
 using ModOrganizer.Json.Penumbra.Groups;
+using ModOrganizer.Tests.Dalamuds.PluginLogs;
 using ModOrganizer.Tests.Json.Penumbra.Groups.Bases;
 using System.Text.Json;
 
@@ -50,6 +53,59 @@ public class TestGroupCombiningReader
     [TestMethod]
     public void TestTryReadWithInvalidBaseGroup()
     {
+        var observer = new StubObserver();
 
+        var groupCombiningReader = new GroupCombiningReaderBuilder()
+            .WithPluginLogDefaults()
+            .WithPluginLogObserver(observer)
+            .WithGroupBaseReaderTryRead(null)
+            .Build();
+
+        var element = JsonSerializer.SerializeToElement(new Dictionary<string, object?>());
+
+        var success = groupCombiningReader.TryRead(element, out var actualGroup);
+
+        Assert.IsFalse(success);
+        Assert.IsNull(actualGroup);
+
+        var calls = observer.GetCalls();
+        Assert.HasCount(1, calls);
+
+        AssertPluginLog.MatchObservedCall(calls[0], nameof(IPluginLog.Debug),
+            actualMessage => Assert.AreEqual($"Failed to read base [Group] for [GroupCombining]: {element}", actualMessage));
+    }
+
+    [TestMethod]
+    public void TestTryReadWithInvalidType()
+    {
+        var observer = new StubObserver();
+
+        var name = "Group Name";
+        var type = "Invalid Type";
+
+        var group = new Group()
+        {
+            Name = name,
+            Type = type
+        };
+
+        var groupCombiningReader = new GroupCombiningReaderBuilder()
+            .WithPluginLogDefaults()
+            .WithPluginLogObserver(observer)
+            .WithGroupBaseReaderTryRead(group)
+            .Build();
+
+        var element = JsonSerializer.SerializeToElement(new Dictionary<string, object?>());
+
+        var success = groupCombiningReader.TryRead(element, out var actualGroup);
+
+        Assert.IsFalse(success);
+        Assert.IsNull(actualGroup);
+
+        var calls = observer.GetCalls();
+        Assert.HasCount(1, calls);
+
+        AssertPluginLog.MatchObservedCall(calls[0], nameof(IPluginLog.Warning),
+            actualMessage => Assert.StartsWith($"Failed to read [GroupCombining], invalid type [{type}] (expected: Combining): {element}", actualMessage));
     }
 }
