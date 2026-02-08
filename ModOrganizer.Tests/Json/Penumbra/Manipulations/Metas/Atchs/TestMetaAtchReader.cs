e17638c20839b0d0e0acaf26330268eaa97f4900
anya-hichu <anya_hichu@outlook.com>
2026-02-08 21:48:34 +0100

    stuff

---
+++ ModOrganizer.Tests/Json/Penumbra/Manipulations/Metas/Atchs/TestMetaAtchReader.cs
@@ -0,0 +1,11 @@
+namespace ModOrganizer.Tests.Json.Penumbra.Manipulations.Metas.Atchs;
+
+[TestClass]
+public class TestMetaAtchReader
+{
+    [TestMethod]
+    public void TestTryRead()
+    {
+
+    }
+}
---
+++ ModOrganizer.Tests/Json/Penumbra/Manipulations/Metas/Atchs/TestMetaAtchWrapperReader.cs
@@ -0,0 +1,11 @@
+namespace ModOrganizer.Tests.Json.Penumbra.Manipulations.Metas.Atchs;
+
+[TestClass]
+public class TestMetaAtchWrapperReader
+{
+    [TestMethod]
+    public void TestTryRead()
+    {
+
+    }
+}
