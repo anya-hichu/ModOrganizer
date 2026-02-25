4de28971ef02ae6605607fc69eff1991b739ff37
anya_hichu <anya_hichu@outlook.com>
2026-02-25 21:27:07 +0100

    tests

--- ModOrganizer.Tests/Json/Penumbra/Manipulations/Metas/Atchs/TestMetaAtchReader.cs
+++ ModOrganizer.Tests/Json/Penumbra/Manipulations/Metas/Atchs/TestMetaAtchReader.cs
@@ -11,21 +11,21 @@ namespace ModOrganizer.Tests.Json.Penumbra.Manipulations.Metas.Atchs;
 [TestClass]
 public class TestMetaAtchReader
 {
+    private static readonly MetaAtchEntry DEFAULT_ENTRY = new()
+    {
+        Bone = string.Empty,
+        Scale = default,
+        OffsetX = default,
+        OffsetY = default,
+        OffsetZ = default,
+        RotationX = default,
+        RotationY = default,
+        RotationZ = default
+    };
+
     [TestMethod]
     public void TestTryRead()
     {
-        var entry = new MetaAtchEntry()
-        {
-            Bone = string.Empty,
-            Scale = default,
-            OffsetX = default,
-            OffsetY = default,
-            OffsetZ = default,
-            RotationX = default,
-            RotationY = default,
-            RotationZ = default
-        };
-
         var gender = "Gender";
         var race = "Race";
         var type = "Type";
@@ -41,7 +41,7 @@ public class TestMetaAtchReader
         });
 
         var reader = new MetaAtchReaderBuilder()
-            .WithMetaAtchEntryReaderTryRead(entry)
+            .WithMetaAtchEntryReaderTryRead(DEFAULT_ENTRY)
             .Build();
 
         var success = reader.TryRead(element, out var metaAtch);
@@ -49,7 +49,7 @@ public class TestMetaAtchReader
         Assert.IsTrue(success);
         Assert.IsNotNull(metaAtch);
 
-        Assert.AreSame(entry, metaAtch.Entry);
+        Assert.AreSame(DEFAULT_ENTRY, metaAtch.Entry);
         Assert.AreEqual(gender, metaAtch.Gender);
         Assert.AreEqual(race, metaAtch.Race);
         Assert.AreEqual(type, metaAtch.Type);
@@ -106,5 +106,78 @@ public class TestMetaAtchReader
 
         AssertPluginLog.MatchObservedCall(calls[0], nameof(IPluginLog.Debug),
             actualMessage => Assert.AreEqual($"Failed to read [MetaAtchEntry] for [MetaAtch]: {element}", actualMessage));
+    }
+
+    [TestMethod]
+    public void TestTryReadWithInvalidType()
+    {
+        var observer = new StubObserver();
+
+        var element = JsonSerializer.SerializeToElement(new Dictionary<string, object?>()
+        {
+            { nameof(MetaAtch.Entry), new Dictionary<string, object?>() },
+            { nameof(MetaAtch.Gender), "Gender" },
+            { nameof(MetaAtch.Race), "Race" },
+            { nameof(MetaAtch.Type), string.Empty },
+            { nameof(MetaAtch.Index), 1 }
+        });
+
+        var reader = new MetaAtchReaderBuilder()
+            .WithPluginLogDefaults()
+            .WithPluginLogObserver(observer)
+            .WithMetaAtchEntryReaderTryRead(DEFAULT_ENTRY)
+            .Build();
+
+        var success = reader.TryRead(element, out var metaAtch);
+
+        Assert.IsFalse(success);
+        Assert.IsNull(metaAtch);
+
+        var calls = observer.GetCalls();
+        Assert.HasCount(2, calls);
+
+        AssertPluginLog.MatchObservedCall(calls[0], nameof(IPluginLog.Debug),
+            actualMessage => Assert.AreEqual("Expected value to not be empty", actualMessage));
+        AssertPluginLog.MatchObservedCall(calls[1], nameof(IPluginLog.Warning),
+            actualMessage => Assert.AreEqual($"Expected property [Type] value to not be empty [String]: {element}", actualMessage));
+    }
+
+    [TestMethod]
+    [DataRow(nameof(MetaAtch.Gender))]
+    [DataRow(nameof(MetaAtch.Race))]
+    [DataRow(nameof(MetaAtch.Index))]
+    public void TestTryReadWithMissingProperty(string propertyName)
+    {
+        var observer = new StubObserver();
+
+        var value = new Dictionary<string, object?>()
+        {
+            { nameof(MetaAtch.Entry), new Dictionary<string, object?>() },
+            { nameof(MetaAtch.Gender), "Gender" },
+            { nameof(MetaAtch.Race), "Race" },
+            { nameof(MetaAtch.Type), "Type" },
+            { nameof(MetaAtch.Index), 1 }
+        };
+
+        value.Remove(propertyName);
+
+        var element = JsonSerializer.SerializeToElement(value);
+
+        var reader = new MetaAtchReaderBuilder()
+            .WithPluginLogDefaults()
+            .WithPluginLogObserver(observer)
+            .WithMetaAtchEntryReaderTryRead(DEFAULT_ENTRY)
+            .Build();
+
+        var success = reader.TryRead(element, out var metaAtch);
+
+        Assert.IsFalse(success);
+        Assert.IsNull(metaAtch);
+
+        var calls = observer.GetCalls();
+        Assert.HasCount(1, calls);
+
+        AssertPluginLog.MatchObservedCall(calls[0], nameof(IPluginLog.Warning),
+            actualMessage => Assert.AreEqual($"Expected property [{propertyName}] to be present: {element}", actualMessage));
     }
 }
